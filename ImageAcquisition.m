fda76e4a5c53a6b7f46c466646eea7f62cbe487c
Tom Riis <tomriis11@gmail.com>
2019-06-27 14:13:33 -0600

    vantage manual script for acquisition and processing

---
+++ ImageAcquisition.m
@@ -0,0 +1,220 @@
+% - Asynchronous plane wave transmit acquisition into multiple
+% RcvBuffer frames with reconstruction and image processing.
+clear all
+V64 = 0; % set to 1 for Vantage 64 system.
+startDepth = 5;
+endDepth = 200;
+% Specify system parameters
+Resource.Parameters.numTransmit = 128; % no. of transmit channels.
+Resource.Parameters.numRcvChannels = 128-(64*V64); % no. of receive channels.
+Resource.Parameters.speedOfSound = 1540; % speed of sound in m/sec
+Resource.Parameters.fakeScanhead = 1; % optional (if no L11-4v)
+Resource.Parameters.simulateMode = 0; % runs script with hardware
+% Specify media points
+pt1; % use predefined collection of media points
+% Specify Trans structure array.
+Trans.name = 'L11-4v';
+Trans.units = 'mm';
+Trans.frequency = 6.25; % not needed if using default center frequency
+Trans = computeTrans(Trans); % L7-4 transducer is 'known' transducer.
+% Specify PData structure array.
+PData.PDelta = [Trans.spacing,0,0.5]; % x, y and z pixel deltas
+PData.Size(1) = ceil((endDepth-startDepth)/PData.PDelta(3));
+PData.Size(2) = ceil(128*Trans.spacing/PData.PDelta(1));
+PData.Size(3) = 1; % 2D image plane
+% PData.Origin is the location [x,y,z] of the upper lft corner of the array.
+PData.Origin = [-63.5*Trans.spacing,0,startDepth];
+PData.Region = computeRegions(PData);
+% Specify Resource buffers.
+% Specify Resource buffers.
+Resource.RcvBuffer(1).datatype = 'int16';
+Resource.RcvBuffer(1).rowsPerFrame = 4096; % this allows for 1/4 maximum range
+Resource.RcvBuffer(1).colsPerFrame = 128;
+Resource.RcvBuffer(1).numFrames = 10; % minimum size is 1 frame.
+Resource.InterBuffer(1).numFrames = 1; % InterBuffer needed for V64=1
+Resource.ImageBuffer(1).numFrames = 10;
+Resource.DisplayWindow(1).Title = 'L11-4v Plane Wave Transmit';
+Resource.DisplayWindow(1).pdelta = 0.3;
+Resource.DisplayWindow(1).Position = [250,150, ... % lower lft corner pos.
+ceil(PData.Size(2)*PData.PDelta(1)/Resource.DisplayWindow(1).pdelta), ...
+ceil(PData.Size(1)*PData.PDelta(3)/Resource.DisplayWindow(1).pdelta)];
+Resource.DisplayWindow(1).ReferencePt = [PData.Origin(1),0,PData.Origin(3)];
+Resource.DisplayWindow(1).AxesUnits = 'mm';
+Resource.DisplayWindow(1).Colormap = gray(256);
+Resource.DisplayWindow(1).numFrames = 20;
+% Specify Transmit waveform structure.
+TW(1).type = 'parametric';
+TW(1).Parameters = [6.25,0.67,2,1]; % A, B, C, D
+% Specify TX structure array.
+TX(1).waveform = 1; % use 1st TW structure.
+TX(1).focus = 0;
+TX(1).Apod = ones(1,Trans.numelements);
+TX(1).Delay = computeTXDelays(TX(1));
+% Specify TGC Waveform structure.
+TGC(1).CntrlPts = [300,450,575,675,750,800,850,900];
+TGC(1).rangeMax = 200;
+TGC(1).Waveform = computeTGCWaveform(TGC);
+% Specify Receive structure array -
+Receive = repmat(struct(...
+'Apod', zeros(1,128), ...
+'startDepth', 0, ...
+'endDepth', 200, ...
+'TGC', 1, ...
+'mode', 0, ...
+'bufnum', 1, ...
+'framenum', 1, ...
+'acqNum', 1, ...
+'sampleMode', 'NS200BW', ...
+'LowPassCoef',[],...
+'InputFilter',[]),...
+1,Resource.RcvBuffer(1).numFrames*(V64+1));
+% - Set event specific Receive attributes.
+if (V64==0)
+    for i = 1:Resource.RcvBuffer(1).numFrames
+        Receive(i).Apod = ones(1,128);
+        Receive(i).framenum = i;
+    end
+else
+    for i = 1:Resource.RcvBuffer(1).numFrames
+        % -- 1st synthetic aperture acquisition for full frame.
+        Receive(2*i-1).Apod(1:64) = 1.0;
+        Receive(2*i-1).framenum = i;
+        Receive(2*i-1).acqNum = 1;
+        % -- 2nd synthetic aperture acquisition for full frame.
+        Receive(2*i).Apod(65:128) = 1.0;
+        Receive(2*i).framenum = i;
+        Receive(2*i).acqNum = 2; % two acquisitions per frame
+    end
+end
+% Specify Recon structure array for 2 board system.
+Recon = struct('senscutoff', 0.6, ...
+'pdatanum', 1, ...
+'rcvBufFrame', -1, ... % use most recently transferred frame
+'IntBufDest', [1,1], ... % needed if V64 = 1
+'ImgBufDest', [1,-1], ... % auto-increment ImageBuffer each recon
+'RINums', 1);
+if V64==0
+    % Define ReconInfo structure for a 128 channel system.
+    ReconInfo(1) = struct('mode','replaceIntensity', ...
+    'txnum', 1, ...
+    'rcvnum', 1, ...
+    'regionnum',1);
+else
+    % Define ReconInfo structure for a 64 channel system.
+    Recon(1).RINums = [1;2];
+    ReconInfo(1) = struct('mode', 'accumIQ', ...
+    'txnum', 1, ...
+    'rcvnum', 1, ...
+    'regionnum', 1);
+    ReconInfo(2) = struct('mode','accumIQ_replaceIntensity', ...
+    'txnum', 1, ...
+    'rcvnum', 2, ...
+    'regionnum',1);
+end
+% Specify processing events.
+pers = 30;
+Process(1).classname = 'Image';
+Process(1).method = 'imageDisplay';
+Process(1).Parameters = {'imgbufnum',1,... % number of buffer to process.
+                        'framenum',-1,... % (-1 => lastFrame)
+                        'pdatanum',1,... % number of PData structure
+                        'pgain',1.0,... % image processing gain
+                        'reject',3,... % see text
+                        'persistMethod','simple',... % ‘simple’ or ‘dynamic’
+                        'persistLevel', pers,...
+                        'grainRemoval', 'none',... % ‘low’,‘medium’,‘high’
+                        'processMethod', 'none',... % see text
+                        'averageMethod', 'none',... % see text
+                        'compressMethod', 'power',... % ‘power’ or ‘log’
+                        'compressFactor', 40,...
+                        'mappingMethod', 'full',... % see text
+                        'display',1,... % display image after processing
+                        'displayWindow',1}; % number of displayWindow to use
+Process(2).classname = 'External';
+Process(2).method = 'myProcFunction';
+Process(2).Parameters = {'srcbuffer','receive',... % name of buffer to process.
+'srcbufnum',1,...
+'srcframenum',-1,... % process the most recent frame.
+'dstbuffer','none'};
+% Specify sequence events.
+SeqControl(1).command = 'timeToNextAcq';
+SeqControl(1).argument = 10000;
+SeqControl(2).command = 'jump';
+SeqControl(2).argument = 1;
+SeqControl(3).command = 'timeToNextAcq'; % time between syn. aper. acquisitions
+SeqControl(3).argument = 500;
+nsc = 4; % start index for new SeqControl
+n = 1; % start index for Events
+for i = 1:Resource.RcvBuffer(1).numFrames
+    Event(n).info = 'Acquire RF Data.';
+    Event(n).tx = 1; % use 1st TX structure.
+    Event(n).rcv = (V64+1)*i-V64; % use 1st Rcv structure for frame.
+    Event(n).recon = 0; % no reconstruction.
+    Event(n).process = 0; % no processing
+    if V64==1
+        Event(n).seqControl = 3;
+        n = n+1;
+        Event(n).info = 'Acquire RF Data for 2nd half of aperture.';
+        Event(n).tx = 1; % use 1st TX structure.
+        Event(n).rcv = 2*i; % use Rcv structure for frame.
+        Event(n).recon = 0; % no reconstruction.
+        Event(n).process = 0; % no processing
+    end
+    Event(n).seqControl = [1,nsc]; % time between frames and transfer
+    SeqControl(nsc).command = 'transferToHost';
+    nsc = nsc + 1;
+    n = n+1;
+    Event(n).info = 'Call external Processing function.';
+    Event(n).tx = 0; % no TX structure.
+    Event(n).rcv = 0; % no Rcv structure.
+    Event(n).recon = 0; % no reconstruction.
+    Event(n).process = 2; % call ext. processing function
+    Event(n).seqControl = 0;
+    n = n+1;
+    Event(n).info = 'Perform reconstruction and image display processing.';
+    Event(n).tx = 0; % no TX structure.
+    Event(n).rcv = 0; % no Rcv structure.
+
+    Event(n).recon = 1; % reconstruction.
+    Event(n).process = 1; % call image processing function
+    Event(n).seqControl = 0;
+    n = n+1;
+end
+Event(n).info = 'Jump back to Event 1.';
+Event(n).tx = 0; % no TX structure.
+Event(n).rcv = 0; % no Rcv structure.
+Event(n).recon = 0; % no reconstruction.
+Event(n).process = 0; % no processing
+Event(n).seqControl = 2; % jump back to Event 1.
+% - Create UI controls for channel selection
+nr = Resource.Parameters.numRcvChannels;
+UI(1).Control = {'UserB1','Style','VsSlider',...
+'Label','Plot Channel',...
+'SliderMinMaxVal',[1,128-64*V64,64-32*V64],...
+'SliderStep', [1/nr,8/nr],...
+'ValueFormat', '%3.0f'};
+UI(1).Callback = {'assignin(''base'',''myPlotChnl'',round(UIValue))'};
+EF(1).Function = text2cell('%EF#1');
+% Save all the structures to a .mat file.
+save('L11-4vAcquireRF');
+return
+
+%EF#1
+myProcFunction(RData)
+persistent myHandle
+% If ‘myPlotChnl’ exists, read it for the channel to plot.
+if evalin('base','exist(''myPlotChnl'',''var'')')
+channel = evalin('base','myPlotChnl');
+else
+channel = 32; % Channel no. to plot
+end
+% Create the figure if it doesn’t exist.
+if isempty(myHandle)||~ishandle(myHandle)
+figure;
+myHandle = axes('XLim',[0,1500],'YLim',[-4096 4096], ...
+'NextPlot','replacechildren');
+end
+% Plot the RF data.
+plot(myHandle,RData(:,channel));
+drawnow
+%EF#1
